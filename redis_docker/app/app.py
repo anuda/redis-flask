a222f513f3da69af969ac23a95c2ddd5ddc3561c
Anurag <anurag2das@gmail.com>
2021-03-16 01:36:21 +0530

    build app via dockerfile

--- redis_docker/app/app.py
+++ redis_docker/app/app.py
@@ -1,11 +1,16 @@
 import time
-
+from loguru import logger
 import redis
-from flask import Flask
-
+from flask import Flask, request
+from celery import Celery
 
 app = Flask(__name__)
+
+
+
+
 cache = redis.Redis(host='redis', port=6379)
+
 
 def get_hit_count():
     retries = 5
@@ -20,5 +25,9 @@ def get_hit_count():
 
 @app.route('/')
 def hello():
+    logger.debug('inside hello')
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! {} times.\n'.format(count)
+
+
+
